3f901b755f6c0faf88603dba8e8f2d8ce4230483
PlagueFencer <pavlopolish@proton.me>
2026-03-05 09:59:49 +0200

    fixes #1: Replace request-promise with node-fetch for fetching jokes.

--- src/joke.js
+++ src/joke.js
@@ -1,18 +1,18 @@
-const request = require("request-promise");
 
-const options = {
-  method: "GET",
-  uri: "https://icanhazdadjoke.com/",
-  headers: {
-    Accept: "application/json",
-    "User-Agent": "Writing JavaScript action GitHub Skills exercise.",
-  },
-  json: true,
+const fetch = require("node-fetch");
+
+
+const url = "https://icanhazdadjoke.com/";
+const headers = {
+  Accept: "application/json",
+  "User-Agent": "Writing JavaScript action GitHub Skills exercise."
 };
 
+
 async function getJoke() {
-  const res = await request(options);
-  return res.joke;
+  const res = await fetch(url, { method: "GET", headers });
+  const data = await res.json();
+  return data.joke;
 }
 
 module.exports = getJoke;
